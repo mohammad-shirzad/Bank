572575df8cf2811faf98752eb0daf9e715201df0
Mohammad Shirzad <mshirzad70@gmail.com>
2020-06-04 00:24:51 +0430

    method for generating PAN and CVV2 is modified

--- BankBusiness/src/main/java/com/bank/business/utility/GeneratorUtil.java
+++ BankBusiness/src/main/java/com/bank/business/utility/GeneratorUtil.java
@@ -1,7 +1,5 @@
 package com.bank.business.utility;
 
-import java.text.DecimalFormat;
-
 public class GeneratorUtil {
     private static final long MIN_PAN_VAL = 1000000000000000L;
     private static final long MAX_PAN_VAL = 9999999999999999L;
@@ -9,10 +7,10 @@ public class GeneratorUtil {
     private static final long MAX_CVV_VAL = 9999L;
 
     public static String generateCardNo() {
-        return new DecimalFormat("###.#####").format((Math.random() * ((MAX_PAN_VAL - MIN_PAN_VAL) + 1)) + MIN_PAN_VAL);
+        return String.valueOf(MIN_PAN_VAL + (long) (Math.random() * (MAX_PAN_VAL - MIN_PAN_VAL)));
     }
 
     public static String generateCVV2() {
-        return new DecimalFormat("###.#####").format((Math.random() * ((MAX_CVV_VAL - MIN_CVV_VAL) + 1)) + MIN_CVV_VAL);
+        return String.valueOf(MIN_CVV_VAL + (long) (Math.random() * (MAX_CVV_VAL - MIN_CVV_VAL)));
     }
 }
