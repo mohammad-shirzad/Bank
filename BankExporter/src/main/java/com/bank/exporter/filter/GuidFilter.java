02172a0010db8cd24780bc21e525884ad10cec7a
Mohammad Shirzad <mshirzad70@gmail.com>
2020-11-27 23:35:01 +0330

    add diagnostic key to logs and change log order of content insertion

---
+++ BankExporter/src/main/java/com/bank/exporter/filter/GuidFilter.java
@@ -0,0 +1,28 @@
+package com.bank.exporter.filter;
+
+import com.common.utils.DiagnosticKeyUtils;
+import org.slf4j.MDC;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.UUID;
+
+public class GuidFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String uuid = UUID.randomUUID().toString();
+        DiagnosticKeyUtils.setDiagKey(uuid);
+        MDC.put("diagKey", uuid);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
